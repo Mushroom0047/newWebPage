c82988614ccd9c56240a19741a638bfefad893ec
Mu$hr00M <71194731+Mushroom0047@users.noreply.github.com>
2021-07-11 19:34:04 -0400

    Edited the knowledge secction !

--- js/app.js
+++ js/app.js
@@ -27,16 +27,3 @@ function openMenu(){
         menuHam.className = "nav-menu";
     }
 }
-
-// Knowledge bar
-let item1 = document.querySelector('.progress-bar-12').style.width = '50%';
-let item2 = document.querySelector('.progress-bar-13').style.width = '80%';
-let item3 = document.querySelector('.progress-bar-14').style.width = '50%';
-let item4 = document.querySelector('.progress-bar-15').style.width = '50%';
-let item5 = document.querySelector('.progress-bar-16').style.width = '90%';
-let item6 = document.querySelector('.progress-bar-17').style.width = '40%';
-let item7 = document.querySelector('.progress-bar-18').style.width = '90%';
-let item8 = document.querySelector('.progress-bar-19').style.width = '10%';
-let item9 = document.querySelector('.progress-bar-20').style.width = '30%';
-let item10 = document.querySelector('.progress-bar-21').style.width = '10%';
-let item11 = document.querySelector('.progress-bar-22').style.width = '30%';
