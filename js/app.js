2028067214164e22b2d17b1f800040cd100df777
Mu <hectorvaldesm47@gmail.com>
2021-12-31 12:54:51 -0300

    Ready all styles modifications

--- js/app.js
+++ js/app.js
@@ -68,3 +68,16 @@ document.querySelector('.up-div').addEventListener('click', ()=>{
         behavior: 'smooth'
     });
 });
+
+//Change the envelope 
+let envelope = document.querySelector('.sobre-icono');
+envelope.addEventListener('mouseenter', ()=>{
+    if(envelope.classList.contains('fa-envelope')){
+        envelope.classList.replace('fa-envelope', 'fa-envelope-open');
+    }
+});
+envelope.addEventListener('mouseleave', ()=>{
+    if(envelope.classList.contains('fa-envelope-open')){
+        envelope.classList.replace('fa-envelope-open','fa-envelope');
+    }
+});
